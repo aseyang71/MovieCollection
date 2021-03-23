b6c069db7c72fff8adfafbc17dcd9bd1e20a1d65
chy_infosys <chy17428@byu.edu>
2021-03-22 19:41:17 -0600

    Trying to get Update work

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -67,10 +67,59 @@ namespace MovieCollection.Controllers
             return RedirectToAction("mvDB");
         }
 
+        [HttpPost]
+        public IActionResult EditMovie(int MovieId)
+        {
+            NewData Movie = _mvCollectionDbContext.NewDatas.First(x => x.MovieId == MovieId);
+
+            //_mvCollectionDbContext.Update(Movie);
+            //_mvCollectionDbContext.SaveChanges();
+            return View();
+        }
+
         public IActionResult mvDB()
         {
             return View (_mvCollectionDbContext.NewDatas);
             //return View(TempStorage.Applications);
+        }
+
+        [HttpPost]
+        public IActionResult SaveChanges(NewData mr, int movieid)
+        {
+            //Identify which movie corresponds to the id passed in 
+            var MovieToOverwrite = _mvCollectionDbContext.NewDatas.Where(mr => mr.MovieId == movieid).FirstOrDefault();
+
+
+
+            //Ensure form inputs are valid
+            if (ModelState.IsValid)
+            {
+                //Update the values of the movie corresponding with the movieid passed in
+                MovieToOverwrite.Title = mr.Title;
+                MovieToOverwrite.Category = mr.Category;
+                MovieToOverwrite.Year = mr.Year;
+                MovieToOverwrite.Director = mr.Director;
+                MovieToOverwrite.Rating = mr.Rating;
+                MovieToOverwrite.Edited = mr.Edited;
+                MovieToOverwrite.LentTo = mr.LentTo;
+                MovieToOverwrite.Notes = mr.Notes;
+
+
+
+                //Save changes to the DB
+                _mvCollectionDbContext.SaveChanges();
+
+
+
+                //Route to view movies page; exclude Independence Day from being displayed
+                return View("ViewMovies", _mvCollectionDbContext.NewDatas.Where(m => m.Title != "Independence Day"));
+            }
+
+
+
+            //If model is invalid, return to same page - show validation violations
+            //Not sure how to repass original movie data...
+            return View("mvDB", mr);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
